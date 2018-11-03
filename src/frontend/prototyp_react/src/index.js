d42cd25973c8a2ea8855cb4de908e0011618c8e5
Cornell Ziepel <Anonymus1404@gmail.com>
2018-11-03 13:15:55 +0100

    some first tests with forms and react

--- src/frontend/prototyp_react/src/index.js
+++ src/frontend/prototyp_react/src/index.js
@@ -1,12 +1,80 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function InputField(props) {
+    return (
+        <input defaultValue={props.value} onChange={(evt)=>props.onChange(evt.target.value, props.index)}></input>
+    )
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Form extends Component {
+    renderInputFields(){
+        let inputs = 
+            this.props.inputs.map(
+                (input, i) => (<InputField  key={i} index={i} value={input.value} onChange={(value, i) => this.props.onChange(value, i)}/>) 
+            )
+        return inputs;
+    }
+  render(){
+    return (
+        <div className="form">
+            {this.renderInputFields()}
+            <button name="submit" onClick={() => this.props.onSubmit()}>Button</button>
+        </div>
+    );
+  }
+}
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            inputs: [
+                {
+                    name: "test",
+                    value: "test",
+                },
+                {
+                    name: "test",
+                    value: "test",
+                },
+            ],
+            test: true,};
+    }
+
+    handleChange(value, i){
+        console.log('change');
+        console.log(value + ' ' + i);
+        let inputs = this.state.inputs;
+        inputs[i].value = "test";
+        this.setState(
+            {
+                inputs: inputs,
+                test: true,
+            }
+        );
+        return
+    }
+    handleSubmit(){
+        console.log('submit');
+        return
+    }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        </header>
+        <main>
+            <Form inputs={this.state.inputs} name="test" onChange={(value, i) => this.handleChange(value, i)} onSubmit={() => this.handleSubmit()}/>
+        </main>
+      </div>
+    );
+  }
+}
+
+  // ========================================
+  
+  ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+  );
