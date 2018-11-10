6d4b67d4a35e9f7883486ffca89f8d0b563dacac
Cornell Ziepel <Anonymus1404@gmail.com>
2018-11-10 01:03:07 +0100

    made prototyp ready for deployment

---
+++ src/frontend/prototyp_react/build/precache-manifest.9f52436d63df8ce8c92e58d4c615b49d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3eb1e6e7b5fe5ee33197",
+    "url": "/static/js/main.3eb1e6e7.chunk.js"
+  },
+  {
+    "revision": "623c0d38defcb39808e5",
+    "url": "/static/js/1.623c0d38.chunk.js"
+  },
+  {
+    "revision": "3eb1e6e7b5fe5ee33197",
+    "url": "/static/css/main.a21d26a0.chunk.css"
+  },
+  {
+    "revision": "98c01c53ed14df2994695a4ad8d2d2ba",
+    "url": "/index.html"
+  }
+];
