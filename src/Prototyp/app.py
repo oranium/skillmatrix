ecdddf53d58f910646f1931cb4a4ca7623bed916
Aron Gaden <aron.gaden@gmail.com>
2018-11-11 20:27:02 +0100

    now use flaskext.mysql

--- src/Prototyp/app.py
+++ src/Prototyp/app.py
@@ -1,17 +1,15 @@
-from flask import Flask, render_template, request, redirect
-from flask_mysqldb import MySQL
-import yaml
+from flask import Flask, render_template, json, request, redirect
+from flaskext.mysql import MySQL
 
+mysql = MySQL()
 app = Flask(__name__)
 
-# Configure db
-db = yaml.load(open('db.yaml'))
-app.config['MYSQL_HOST'] = db['mysql_host']
-app.config['MYSQL_USER'] = db['mysql_user']
-app.config['MYSQL_PASSWORD'] = db['mysql_password']
-app.config['MYSQL_DB'] = db['mysql_db']
-
-mysql = MySQL(app)
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Momomomo2'
+app.config['MYSQL_DATABASE_DB'] = 'test'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -19,20 +17,22 @@ def index():
         # Fetch form data
         userDetails = request.form
         name = userDetails['name']
-        email = userDetails['email']
-        cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO users(name, email) VALUES(%s, %s)",(name, email))
-        mysql.connection.commit()
-        cur.close()
+        passwort = userDetails['email']
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO Daddy(name, passwort) VALUES(%s, %s)",(name, passwort))
+        conn.commit()
+        cursor.close()
         return redirect('/users')
     return render_template('index.html')
 
 @app.route('/users')
 def users():
-    cur = mysql.connection.cursor()
-    resultValue = cur.execute("SELECT * FROM users")
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    resultValue = cursor.execute("SELECT * FROM Daddy")
     if resultValue > 0:
-        userDetails = cur.fetchall()
+        userDetails = cursor.fetchall()
         return render_template('users.html',userDetails=userDetails)
 
 if __name__ == '__main__':
