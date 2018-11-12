34a26a586ccdb0242fcfa454e914868868385c89
Aron Gaden <aron.gaden@gmail.com>
2018-11-12 19:00:58 +0100

    added build

---
+++ src/frontend/build/precache-manifest.ced25e0e346a0cc656269c0ce8a5dbbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "037067a10d80ab848b85",
+    "url": "/static/js/main.037067a1.chunk.js"
+  },
+  {
+    "revision": "22311baf52291f795004",
+    "url": "/static/js/1.22311baf.chunk.js"
+  },
+  {
+    "revision": "037067a10d80ab848b85",
+    "url": "/static/css/main.54daa8ad.chunk.css"
+  },
+  {
+    "revision": "02984e0cc1f474c01c4eb92c2375881c",
+    "url": "/index.html"
+  }
+];
