9e0c5f0a2e7c201a7a44b2cbbfbd84b62953960d
Aron Gaden <aron.gaden@mailbox.tu-dresden.de>
2018-11-12 20:55:50 +0100

    rebuild build

---
+++ src/frontend/build/precache-manifest.e4ad737af4e37398c3ea3e89a6689765.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "ceea054843a7d417b332",
+    "url": "./static/js/main.ceea0548.chunk.js"
+  },
+  {
+    "revision": "580574ee02ac5d11874d",
+    "url": "./static/js/1.580574ee.chunk.js"
+  },
+  {
+    "revision": "ceea054843a7d417b332",
+    "url": "./static/css/main.6513979a.chunk.css"
+  },
+  {
+    "revision": "8af1494e11c3335ffa7685a9b43ba49c",
+    "url": "./index.html"
+  }
+];
--- src/frontend/build/service-worker.js
+++ src/frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ced25e0e346a0cc656269c0ce8a5dbbc.js"
+  "./precache-manifest.e4ad737af4e37398c3ea3e89a6689765.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
