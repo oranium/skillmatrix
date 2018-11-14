ce99a13810f17498385f364bff64a76f148c0bff
Aron Gaden <aron.gaden@gmail.com>
2018-11-14 21:40:44 +0100

    add test_login.py

--- backend/test/test_login.py
+++ backend/test/test_login.py
@@ -9,7 +9,16 @@ class TestLogin(unittest.TestCase):
         self.empty_string = ""
         self.wrong_credentials = "anything"
         self.correct_credentials_user =  "AzureAD.SWT.com\ADadmin"
-        self.correct_credentials_pass
+        self.correct_credentials_pass = "@Administrator"    
 
-    def test_empty_string():
-        self.assertIsInstance(LDAPUnknownAuthenticationMethodError,AuthenticationController.login(empty_string,empty_string))
+    def test_login_empty_string():
+        self.assertEqual("Please check if username and password were submitted.",AuthenticationController.login(self.empty_string,empty_string))
+
+    def test_login_timeout():
+        self.assertEqual("Timeout. Check connection to Active Directory service.",AuthenticationController.login(self.empty_string,"timeout"))
+    
+    def test_login_wrong_credentials():
+        self.assertEqual("Login unsuccessful. Please check username and password.",AuthenticationController.login(self.wrong_credentials,wrong_credentials))
+    
+    def test_login_correct_credentials():
+        self.assertEqual("Logged in.",AuthenticationController.login(self.correct_credentials_user,self.correct_credentials_pass))
