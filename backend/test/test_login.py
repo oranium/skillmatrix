4bb46a15d8f9fc63703fb0652035914ccda50b96
Aron Gaden <aron.gaden@gmail.com>
2018-11-16 10:49:18 +0100

    add empty mock ldap/AD to test_login

--- backend/test/test_login.py
+++ backend/test/test_login.py
@@ -1,24 +1,54 @@
+from src import authentication
 import unittest
-#
+import json
+import sys
+# , ALL_ATTRIBUTES, NTLM
+from ldap3 import Server, Connection, MOCK_SYNC, OFFLINE_AD_2012_R2
 '''
-Currently, the mock AD assumes the correct credentials to be : name = AzureAD.SWT.com\ADadmin passwd = @Administrator 
+MOCK LDAP doc here: https://ldap3.readthedocs.io/mocking.html
+
 '''
-class TestLogin(unittest.TestCase):
+
+# makes importing authentication possible
+sys.path.append("../")
+
+# setup mock server
+server = Server('mock_ad')
+connection = Connection(server, user='cn=mock_user,ou=test,o=lab',
+                        password='mock_password', client_strategy=MOCK_SYNC)
+
+
+class TestAuthentication(unittest.TestCase):
 
     def set_up(self):
-        self.empty_string = ""
-        self.wrong_credentials = "anything"
-        self.correct_credentials_user =  "AzureAD.SWT.com\ADadmin"
-        self.correct_credentials_pass = "@Administrator"    
+        pass
 
-    def test_login_empty_string():
-        self.assertEqual("Please check if username and password were submitted.",AuthenticationController.login(self.empty_string,empty_string))
+    def test_login_empty_string(self):
+        pass
 
-    def test_login_timeout():
-        self.assertEqual("Timeout. Check connection to Active Directory service.",AuthenticationController.login(self.empty_string,"timeout"))
-    
-    def test_login_wrong_credentials():
-        self.assertEqual("Login unsuccessful. Please check username and password.",AuthenticationController.login(self.wrong_credentials,wrong_credentials))
-    
-    def test_login_correct_credentials():
-        self.assertEqual("Logged in.",AuthenticationController.login(self.correct_credentials_user,self.correct_credentials_pass))
+    def test_login_timeout(self):
+        pass
+
+    def test_login_wrong_credentials(self):
+        pass
+
+    def test_login_correct_credentials(self):
+        pass
+
+    def test_logout_successful(self):
+        pass
+
+    def test_logout_fail(self):
+        pass
+
+
+class TestAPI(unittest.TestCase):
+
+    def set_up(self):
+        pass
+
+    def test_login_api(self):
+        pass
+
+    def test_logout_api(self):
+        pass
