379500e1ea5f7562827d01f392b04305c4b8091a
wpertsch <willy.pertsch@gmail.com>
2018-11-20 16:14:45 +0100

    Aufräumen

--- src/frontend/react_prototype/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-// reducers >> index.js
-import { combineReducers } from 'redux';
-import {formState, user, page} from './reducers';
-
-export default combineReducers({
-    formState,
-    page,
-    user
-})
