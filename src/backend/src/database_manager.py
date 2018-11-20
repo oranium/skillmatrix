9eb1c93d03c6b41692a8af5bcaae72d7a0b625cc
wpertsch <willy.pertsch@gmail.com>
2018-11-20 18:21:20 +0100

    sql-alchemy config
    is not working, i dont know why

--- src/backend/src/database_manager.py
+++ src/backend/src/database_manager.py
@@ -2,49 +2,60 @@ from flask import Flask, render_template, json, request, redirect
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Momomomo2@localhost/pre'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Momomomo2@localhost/sm1'
 api = Api(app)
 db = SQLAlchemy(app)
 
-class Profile(db.Model):
+class user(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(45), unique=True, nullable=False)
+    username = db.Column(db.String(45), nullable=False)
     surename = db.Column(db.String(45), nullable=False)
     forename = db.Column(db.String(45), nullable=False)
     place = db.Column(db.String(45), nullable=True)
-    #backref gibts noch nicht
-    skill = db.relationship('Skill', backref='author', lazy=True, nullable=True)
+#    skill = db.relationship('Skill', backref='author', lazy=True, nullable=True)
 
     def __repr__(self):
-        return f"Profile('{self.forename}', '{self.surename}')"
+        return '<user %r>' % self.username
 
+db.drop_all()
+db.create_all()
 
-class Skill(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(127), nullable=False)
-    level = db.Column(db.Integer, nullable=False)
+willy = user(username='willyadmin', surename ='Pertsch', forename = 'Wilhelm', place = 'wund')
+aron = user(username='aronnormal', surename ='Gaden', forename = 'Aron', place = 'blau')
+
+db.session.add(willy)
+db.session.add(aron)
+user.query.all()
+user.query.filter_by(username='willy').first()
+
+#class Skill(db.Model):
+#    id = db.Column(db.Integer, primary_key=True)
+#    name = db.Column(db.String(127), nullable=False)
+#    level = db.Column(db.Integer, nullable=False)
     #date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    description = db.Column(db.Text, nullable=True)
-    profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'), nullable=False)
+#    description = db.Column(db.Text, nullable=True)
+#    profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'), nullable=False)
 
-    def __repr__(self):
-        return f"Post('{self.name}', '{self.level}')"
+#    def __repr__(self):
+#        return f"Post('{self.name}', '{self.level}')"
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'about' : 'Hello World!'}
+#class HelloWorld(Resource):
+#    def get(self):
+#        return {'about' : 'Hello World!'}
+#
+#    def post(self):
+#        some_json = request.get_json()
+#        return {'you sent' : some_json}, 201
 
-    def post(self):
-        some_json = request.get_json()
-        return {'you sent' : some_json}, 201
+#class Multi(Resource):
+#    def get(self, num):
+#        return {'result' : num*10}
 
-class Multi(Resource):
-    def get(self, num):
-        return {'result' : num*10}
-
-api.add_resource(HelloWorld, '/')
-api.add_resource(Multi, '/multi/<int:num>')
+#api.add_resource(HelloWorld, '/')
+#api.add_resource(Multi, '/multi/<int:num>')
 
 if __name__ == '__main__':
     app.run(debug=True)
