2c9aeb0e07be5aa1bf57e439db968b50102d1c2d
oranium <aron.gaden@gmail.com>
2018-11-21 17:48:02 +0100

    return appropriate status code according to login error

--- src/backend/src/rest_api.py
+++ src/backend/src/rest_api.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, Response
+import json
 from flask_restful import Resource, Api, reqparse
 import sys
 import authentication
@@ -13,7 +14,14 @@ class Login(Resource):
         parser.add_argument('username', type=str)
         parser.add_argument('password', type=str)
         args = parser.parse_args()
-        return authentication.Authentication.login(self,args['username'], args['password'])
+        try:
+            return authentication.Authentication.login(self,args['username'], args['password'])
+        except AttributeError:
+            return Response(status=400)
+        except TimeoutError:
+            return Response(status=504)
+        except BaseException:
+            return Response(status=520)
 
 # RESTful API for logout
 class Logout(Resource):
@@ -23,7 +31,17 @@ class Logout(Resource):
         # returns JSON containing username and boolean whether logout was a success (True) or failure (False)
         #return {args['username']: auth.logout(args['username'])}
         #for TDD
-        return {args['username']:False} if args['username'] == "dontwork"  else {args['username']:True}
+        try:
+            return {args['username']:False} if args['username'] == "dontwork"  else {args['username']:True}
+        except AttributeError:
+            return Response(status=400)
+        except TimeoutError:
+            return Response(status=504)
+        except BaseException:
+            return Response(status=520)
 
 api.add_resource(Login, '/login')
 api.add_resource(Logout, '/logout')
+
+if __name__ == "__main__":
+    app.run(debug=True)
