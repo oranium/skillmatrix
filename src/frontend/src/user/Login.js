b65365f6c9e1faa439f2f6535d509bd7ae381ad1
Cornell Ziepel <Anonymus1404@gmail.com>
2018-11-21 17:59:33 +0100

    changed errorHandling for rest api calls #9

--- src/frontend/src/user/Login.js
+++ src/frontend/src/user/Login.js
@@ -1,78 +1,31 @@
 import RestPoints from './Init';
 import RestCom from './Rest';
 
-// translate error codes to error Msg and return a new map that looks like:
-// response = {user: {username: String}, errorMsg: String, success: Bool}
-const errorCodesToErrorMsg = (apiResponse) => {
-  const response = {
-    user: '',
-    errorMsg: '',
-    success: true,
-  };
-
-  // there was an Error connecting:
-  if (apiResponse === undefined) {
-    response.errorMsg = 'An error occured while connecting to the database. Check your Internet connection.';
-    response.success = false;
-    return response;
-  }
-
-  const { data } = apiResponse;
-
-  // looks like {user: {username: String, ....}}
-  const { user } = data;
-
-  // holds the error code
-  const { error } = data;
-
-  switch (error) {
-    case 0:
-      response.user = user;
-      response.success = true;
-      break;
-    case 1:
-      response.errorMsg = 'Wrong login credentials.';
-      response.success = false;
-      break;
-    case 2:
-      response.errorMsg = 'Please fill in all form fields.';
-      response.success = false;
-      break;
-    case 3:
-      response.errorMsg = 'Server Timeout. Please try again.';
-      response.success = false;
-      break;
-    default:
-      response.errorMsg = 'An unknown Error occurred. Please try again.';
-      response.success = false;
-      break;
-  }
-  return response;
-};
-
 export default async function (username, password) {
   // map data to get the right data structure for posting to server
-  const user = {
+  const loginCredentials = {
     username,
     password,
   };
 
-  try {
-    const Rest = new RestCom(RestPoints.login, user);
-    // post and get response from server
-    const response = await Rest.post();
+  let response = {};
 
-    return errorCodesToErrorMsg(response);
-  } catch (error) {
-    console.log(error);
+  try {
+    const Rest = new RestCom(RestPoints.login, loginCredentials);
+    // post and get response from server
+    const { data } = await Rest.post();
+    const { user } = data;
+    response = {
+      user,
+      success: true,
+    };
+  } catch (e) {
+    const errorMsg = e.message;
+    response = {
+      errorMsg,
+      success: false,
+    };
   }
 
-  const response = {
-    user: '',
-    errorMsg: '',
-    success: true,
-  };
-  response.errorMsg = 'An unknown Error occurred. Please try again.';
-  response.success = false;
   return response;
 }
--- src/frontend/src/user/Rest.js
+++ src/frontend/src/user/Rest.js
@@ -4,10 +4,16 @@ const config = require('../config.json');
 
 const errorCodesToErrorMsg = (errorCode) => {
   switch (errorCode) {
+    case 400:
+      return 'Wrong login credentials.';
     case 404:
-      return 'Server does not answer.';
+      return 'Couldnt connect to Server. Please try again.';
+    case 504:
+      return 'Active Directory timeout. Please try again.';
+    case 520:
+      return 'An unknown error occured. Please try again.';
     default:
-      return 'Unknown error occured. Please try again.';
+      return 'An unknown error occured. Please try again.';
   }
 };
 
@@ -19,23 +25,21 @@ class RestCom {
 
   async post() {
     const response = await axios.post(this.restApi, this.data).catch((error) => {
+      let errorMsg;
       if (error.response) {
         // The request was made and the server responded with a status code
         // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-        throw errorCodesToErrorMsg(error.response.status);
+        errorMsg = errorCodesToErrorMsg(error.response.status);
       } else if (error.request) {
         // The request was made but no response was received
         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
         // http.ClientRequest in node.js
-        console.log(error.request);
+        errorMsg = 'An error occured while connecting to the server. Please check your Internet connection.';
       } else {
         // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message);
+        errorMsg = errorCodesToErrorMsg(520);
       }
-      console.log(error.config);
+      throw new Error(errorMsg);
     });
 
     return response;
