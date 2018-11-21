ccf5210b343f4cc7a9182427a73b36ee50ce2b06
oranium <aron.gaden@gmail.com>
2018-11-21 19:48:03 +0100

    remove test_login, update test_runner.py

--- src/backend/test/test_runner.py
+++ src/backend/test/test_runner.py
@@ -1,13 +1,15 @@
 import unittest
 #test modules
-import test_login
+import test_authentication
+import test_rest_api
 
 #initialize the test suite
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
 #add tests to suite
-suite.addTests(loader.loadTestsFromModule(test_login))
+suite.addTests(loader.loadTestsFromModule(test_authentication))
+suite.addTests(loader.loadTestsFromModule(test_rest_api))
 
 #run tests
 runner = unittest.TextTestRunner()
