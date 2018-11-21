deeca2156bad1f1708bedbb1558146b6f01c2dbf
wpertsch <willy.pertsch@gmail.com>
2018-11-21 18:07:44 +0100

    database_manager database rest connection
    Datenbank mit SQL-Alchemy funktioniert und kann Abfragen über Restpunkt an senden.
    
    Co-Authored-By: BrnRnhld <brnrnhld@users.noreply.github.com>

--- src/backend/src/database_manager.py
+++ src/backend/src/database_manager.py
@@ -1,6 +1,7 @@
 from flask import Flask, json, request, redirect
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Momomomo2@localhost/sm1'
@@ -14,22 +15,28 @@ class user(db.Model):
     surename = db.Column(db.String(45), nullable=False)
     forename = db.Column(db.String(45), nullable=False)
     place = db.Column(db.String(45), nullable=True)
-#    skill = db.relationship('Skill', backref='author', lazy=True, nullable=True)
+#   skill = db.relationship('Skill', backref='author', lazy=True, nullable=True)
 
     def __repr__(self):
         return '<user %r>' % self.username
-db.drop_all()
-db.create_all()
 
-willy = user(username='willyadmin', surename ='Pertsch', forename = 'Wilhelm', place = 'wund')
-aron = user(username='aronnormal', surename ='Gaden', forename = 'Aron', place = 'blau')
+#db.drop_all()
+#db.create_all()
 
-db.session.add(willy)
-db.session.add(aron)
-user.query.all()
-user.query.filter_by(username='willy').first()
+#willy = user(username='willyadmin', surename ='Pertsch', forename = 'Wilhelm', place = 'wund')
+#aron = user(username='aronnormal', surename ='Gaden', forename = 'Aron', place = 'blau')
+#bruno = user(username='bruenonormal', surename ='Reinhold', forename = 'Bruno', place = 'dresden')
 
-print('all good')
+#db.session.add(willy)
+#db.session.add(aron)
+#db.session.add(bruno)
+#user.query.all()
+#user.query.filter_by(username='willyadmin').first()
+
+#print(user.query.all())
+
+
+
 
 #class Skill(db.Model):
 #    id = db.Column(db.Integer, primary_key=True)
@@ -56,6 +63,27 @@ print('all good')
 
 #api.add_resource(HelloWorld, '/')
 #api.add_resource(Multi, '/multi/<int:num>')
+
+
+class HelloWorld(Resource):
+    print('in hw')
+    db.drop_all()
+    db.create_all()
+
+    willy = user(username='willyadmin', surename ='Pertsch', forename = 'Wilhelm', place = 'wund')
+    aron = user(username='aronnormal', surename ='Gaden', forename = 'Aron', place = 'blau')
+    bruno = user(username='bruenonormal', surename ='Reinhold', forename = 'Bruno', place = 'dresden')
+
+    db.session.add(willy)
+    db.session.add(aron)
+    db.session.add(bruno)
+    data   = user.query.all()
+
+    def get(self):
+        sotr = ''.join(str(e) for e in self.data)
+        return {'results' : sotr}, 201
+
+api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
     app.run()
