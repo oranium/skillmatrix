141aecba229e0dbba1cd23891b604d0d142ff3f6
wpertsch <willy.pertsch@gmail.com>
2018-11-22 19:29:56 +0100

    persistente daten

--- src/backend/src/database_manager.py
+++ src/backend/src/database_manager.py
@@ -8,8 +8,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Momomomo2@localhos
 api = Api(app)
 db = SQLAlchemy(app)
 
-class user(db.Model):
-    __tablename__ = 'user'
+class users(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(45), nullable=False)
     surename = db.Column(db.String(45), nullable=False)
@@ -76,17 +76,23 @@ class session(db.Model):
 
 class HelloWorld(Resource):
     print('in hw')
-    db.drop_all()
-    db.create_all()
+    #db.drop_all()
+    #db.create_all()
 
-    willy = user(username='willyadmin', surename ='Pertsch', forename = 'Wilhelm', place = 'wund')
-    aron = user(username='aronnormal', surename ='Gaden', forename = 'Aron', place = 'blau')
-    bruno = user(username='bruenonormal', surename ='Reinhold', forename = 'Bruno', place = 'dresden')
+    willy = users(username='willynormal', surename ='Pertsch', forename = 'Wilhelm', place = 'wund')
+    #aron = users(username='aronnormal', surename ='Gaden', forename = 'Aron', place = 'blau')
+    bruno = users(username='bruenonormal', surename ='Reinhold', forename = 'Bruno', place = 'dresden')
+    #willy2 = users(username='willynormal2', surename ='Pertsch', forename = 'Wilhelm', place = 'wund')
 
     db.session.add(willy)
-    db.session.add(aron)
+    #db.session.add(aron)
     db.session.add(bruno)
-    data   = user.query.all()
+    #db.session.add(willy2)
+    #delete_this = users.query.filter_by(id=2).first
+    #db.session.delete(delete_this)
+    db.session.commit()
+
+    data = users.query.all()
 
     def get(self):
         sotr = ''.join(str(e) for e in self.data)
