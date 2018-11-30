c71bb2437f5cb3c8fef946e9628a5965a3966b67
Aron Gaden <aron.gaden@gmail.com>
2018-11-30 18:17:05 +0100

    implement singleton

--- src/backend/src/api/login.py
+++ src/backend/src/api/login.py
@@ -3,13 +3,12 @@ import sys
 from flask import Response
 from flask_restful import Resource, reqparse
 
-from src.controller.controller import Controller
+from src.controller.controller import controller
 
 
 class Login(Resource):
     """Login-API deserializes JSON and hands it to the Authentication class of authentication module"""
     def post(self):
-        controller = Controller.get_instance()
         parser = reqparse.RequestParser()
         parser.add_argument("username", type=str)
         parser.add_argument("password", type=str)
--- src/backend/src/api/search.py
+++ src/backend/src/api/search.py
@@ -4,19 +4,18 @@ import json
 from flask import Response
 from flask_restful import Resource, reqparse
 
-from src.controller.controller import Controller
+from src.controller.controller import controller
 
 
 class Search(Resource):
     '''Search-API deserializes the query.
     Takes id of the searching user and an array of skills (currently only one term)'''
     def post(self):
-        controller = Controller.get_instance()
         parser = reqparse.RequestParser()
         parser.add_argument("query", type=str)
         args = parser.parse_args()
         try:
-            message = controller.handle_query(self, args["query"])
+            message = controller.search(self, args["query"])
             print(message, file=sys.stderr)
             return Response(message, status=200, mimetype="application/json")
         except ValueError:
