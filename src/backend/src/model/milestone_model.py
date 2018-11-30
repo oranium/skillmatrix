c78a1e32e6887d278f026c69101124bf0544864b
Aron Gaden <aron.gaden@gmail.com>
2018-11-30 15:56:08 +0100

    add empty model functions

---
+++ src/backend/src/model/milestone_model.py
@@ -0,0 +1,12 @@
+"""Contains model for Logout"""
+from src.model.model import Model
+
+class LogoutModel(Model):
+
+    """This class converts the logged out user to a JSON to hand over to the frontend"""
+
+    def __init__(self,date,comment):
+        pass
+
+    def to_json(self):
+        pass
---
+++ src/backend/src/model/profile_model.py
@@ -0,0 +1,12 @@
+"""Contains model for Profiles"""
+from src.model.model import Model
+
+class ProfileModel(Model):
+
+    """This class converts a user to a JSON to hand over to the frontend"""
+
+    def __init__(self,user,skills):
+        pass
+
+    def to_json(self):
+        pass
