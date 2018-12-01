270e089b8490b5a1bcdabbca137ead631549bf9f
Aron Gaden <aron.gaden@gmail.com>
2018-12-01 17:10:19 +0100

    add all models

--- src/backend/src/model/search_model.py
+++ src/backend/src/model/search_model.py
@@ -1,11 +1,12 @@
 """Contains model for Search"""
 from src.model.model import Model
 
+
 class SearchModel(Model):
 
     """This class converts the search query and results to a JSON to hand over to the frontend"""
 
-    def __init__(self,query,results):
+    def __init__(self, query, results):
         pass
 
     def to_json(self):
