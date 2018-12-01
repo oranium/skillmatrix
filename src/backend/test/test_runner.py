b6c9f6fc1cfcfa50fef121613ebab4629653f6b3
Aron Gaden <aron.gaden@gmail.com>
2018-12-01 17:15:25 +0100

    update test_runner

--- src/backend/test/test_runner.py
+++ src/backend/test/test_runner.py
@@ -1,21 +1,22 @@
 import unittest
 
-#test modules
-from test import test_authentication,test_rest_api,test_database_controller
+# test modules
+from test import test_authentication_controller, test_rest_api, test_database_controller, test_model
 
 
-#initialize the test suite
+# initialize the test suite
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
-#add tests to suite
+# add tests to suite
 
-suite.addTests(loader.loadTestsFromModule(test_authentication))
+suite.addTests(loader.loadTestsFromModule(test_authentication_controller))
 suite.addTests(loader.loadTestsFromModule(test_rest_api))
-#suite.addTests(loader.loadTestsFromModule(test_database_controller))
+suite.addTests(loader.loadTestsFromModule(test_database_controller))
+suite.addTests(loader.loadTestsFromModule(test_model))
 
-#run tests
+# run tests
 runner = unittest.TextTestRunner()
 result = runner.run(suite) 
 
