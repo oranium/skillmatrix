2caa177db4195edc0a2cd334d3833c71f450c112
Aron Gaden <aron.gaden@gmail.com>
2018-12-01 18:16:51 +0100

    implement controller methods

--- src/backend/src/controller/controller.py
+++ src/backend/src/controller/controller.py
@@ -1,22 +1,38 @@
 """Contains the backend controller"""
+from model.logout_model import LogoutModel
+from model.profile_model import ProfileModel
+from model.search_model import SearchModel
+from src.controller.authentication_controller import authentication_controller
+from src.controller.database_controller import database_controller
 
 
 class Controller:
-    """Singleton, calls BackendController and AuthenticationController. Returns JSONs from models to APIs"""
-    def login(self, user, password):
-        pass
+    """Singleton, calls DatabaseController and AuthenticationController. Returns JSONs from models to APIs"""
+    @staticmethod
+    def login(user, password):
+        authentication_controller.login(user, password)
+        user_skills = database_controller.skills(user)
+        return ProfileModel(user, user_skills)
 
-    def logout(self, user):
-        pass
+    @staticmethod
+    def logout(user):
+        authentication_controller.logout(user)
+        return LogoutModel(user)
 
-    def search(self, query):
-        pass
+    @staticmethod
+    def search(query):
+        results = database_controller.search(query)
+        return SearchModel(query, results)
 
-    def set_skills(self, user, skills):
-        pass
+    @staticmethod
+    def set_skills(user, skills):
+        database_controller.set_skills(user, skills)
+        return True
 
-    def add_milestone(self, user, milestone):
-        pass
+    @staticmethod
+    def add_milestone(user, skill, milestone):
+        database_controller.add_milestone(user, skill, milestone)
+        return True
 
 
 controller = Controller()
