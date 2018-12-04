169f86ff576b96a28a2d6be0fef4730e5c518d6b
Aron Gaden <aron.gaden@gmail.com>
2018-12-04 17:47:08 +0100

    comment out all tests that use the database

--- src/backend/test/test_runner.py
+++ src/backend/test/test_runner.py
@@ -1,8 +1,7 @@
 import unittest
 
 # test modules
-from test import test_authentication_controller, test_rest_api, test_controller, test_model, test_db_model
-
+from test import test_model  # , test_controller, test_authentication_controller, test_db_model
 
 # initialize the test suite
 
@@ -10,13 +9,13 @@ loader = unittest.TestLoader()
 suite = unittest.TestSuite()
 
 # add tests to suite
-suite.addTests(loader.loadTestsFromModule(test_authentication_controller))
-suite.addTests(loader.loadTestsFromModule(test_rest_api))
-suite.addTests(loader.loadTestsFromModule(test_controller))
+# suite.addTests(loader.loadTestsFromModule(test_authentication_controller))
+#suite.addTests(loader.loadTestsFromModule(test_rest_api))
+# suite.addTests(loader.loadTestsFromModule(test_controller))
 suite.addTests(loader.loadTestsFromModule(test_model))
-suite.addTests(loader.loadTestsFromModule(test_db_model))
+# suite.addTests(loader.loadTestsFromModule(test_db_model))
 
-#run tests
+# run tests
 runner = unittest.TextTestRunner()
 result = runner.run(suite) 
 
