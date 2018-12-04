1454c5c7dfe9aa2b7d587d342c9c7f3a74c37251
Cornell Ziepel <Anonymus1404@gmail.com>
2018-12-04 23:01:17 +0100

    implement api response prozessing to insert everything into state #83

--- src/frontend/src/components/search/SearchController.js
+++ src/frontend/src/components/search/SearchController.js
@@ -7,15 +7,67 @@ import ControlledExpansionPanels from 'components/common/ControlledExpansionPane
 
 // redux
 import store from 'Store';
-import {
-  setError, setSearchResults, showSearchResults, setSearchError,
-} from 'actions';
+import { setError, setSearchResults, showSearchResults, setSearchError } from 'actions';
 
 // Rest
 import RestPoints from 'rest/Init';
 import RestCom from 'rest/Rest';
+import { addProfile } from 'actions';
 
 class SearchController extends Component {
+  // build query object for api request
+  getSearchQuery = searchValues => {
+    const query = {};
+    // put each search value as key into map
+    // 1 => min Level
+    searchValues.forEach(tag => {
+      query[tag.value] = 1;
+    });
+    return query;
+  };
+
+  prozessSearchResults = data => {
+    const {query, results} = data;
+    const {has_all, has_some} = results;
+    const allProfiles = has_all.concat(has_some);
+    // add all profiles to profile array 
+    allProfiles.forEach(profile => {
+      store.dispatch(addProfile(profile));
+    });
+
+    // build map from skills that only match query
+    const searchResults = {
+      hasAll: [],
+      hasSome: [],
+    }
+    has_all.forEach( profile => {
+      const {skills} = profile;
+      const searchSkills = [];
+      skills.forEach(skill =>{
+        if (query.hasOwnProperty(skill.skillname)){
+          searchSkills.push(skill);
+        }
+      });
+      profile.skills = searchSkills;
+      searchResults.hasAll.push(profile);
+    });
+
+    has_some.forEach( profile => {
+      const {skills} = profile;
+      const searchSkills = [];
+      skills.forEach(skill =>{
+        if (query.hasOwnProperty(skill.skillname)){
+          searchSkills.push(skill);
+        }
+      });
+      profile.skills = searchSkills;
+      searchResults.hasSome.push(profile);
+    });
+
+    store.dispatch(setSearchResults(searchResults));
+    return searchResults;
+  }
+
   // get results for query when user clicks on search button and store them into state
   async handleSearch(e) {
     e.preventDefault();
@@ -26,38 +78,31 @@ class SearchController extends Component {
       store.dispatch(setSearchError(true));
       return;
     }
-    const query = {};
-    // put each search value as key into map
-    // 1 => min Level
-    searchValues.forEach((tag) => {
-      query[tag.value] = 1;
-    });
     const search = {
-      query,
+      query: this.getSearchQuery(searchValues),
     };
     const Rest = new RestCom(RestPoints.search, JSON.stringify(search));
+    // try to send data to api and 
     try {
       const { data } = await Rest.post();
-      const { result } = data;
-      // store results into state
-      store.dispatch(setSearchResults(result));
+      this.prozessSearchResults(data);
       // show results to user
       store.dispatch(showSearchResults);
     } catch (e) {
       store.dispatch(setError(e.message));
-      console.log(e);
     }
+    // remove empty error on submit
+    store.dispatch(setSearchError(false));
   }
 
   render() {
-    const { state, onChange } = this.props;
+    const { state } = this.props;
     const { search, formState } = state;
     const { showResults, results } = search;
     return (
       <div>
         <SearchForm
           searchField={formState.searchfield}
-          onChange={(id, value) => onChange(id, value)}
           onSearch={event => this.handleSearch(event)}
         />
         {showResults && <ControlledExpansionPanels results={results} />}
