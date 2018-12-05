932d4a05f96c3dc152b94a0089e68b85991cbfa0
Cornell Ziepel <Anonymus1404@gmail.com>
2018-12-06 00:45:02 +0100

    search result design #83

--- src/frontend/src/components/search/Panel.js
+++ src/frontend/src/components/search/Panel.js
@@ -3,8 +3,13 @@ import { withStyles } from '@material-ui/core/styles';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
 import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import SkillPanel from 'components/search/SkillPanel';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+import Avatar from '@material-ui/core/Avatar';
 
 const styles = theme => ({
   root: {
@@ -17,8 +22,8 @@ const styles = theme => ({
     flexShrink: 0,
   },
   secondaryHeading: {
-    fontSize: theme.typography.pxToRem(15),
     color: theme.palette.text.secondary,
+    float: 'left',
   },
 });
 
@@ -28,23 +33,25 @@ function Panel(props) {
   } = props;
   console.log(skills);
 
-  const skillsList = [];
-  skills.forEach((skill) => {
-    skillsList.push(<p>{`Skillname: ${skill.skillname}, Level: ${skill.level}`}</p>);
-  });
+  const levelList = skills.map(
+    (skill, i) => `${skill.skillname}: level ${skill.level} ${skills[i + 1] ? ', ' : ''}`,
+  );
   return (
     <ExpansionPanel expanded={isExpanded}>
       <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+        <Avatar>{username[0]}</Avatar>
         <Typography className={classes.heading}>{username}</Typography>
-        <Typography className={classes.secondaryHeading}>
-          {`matches ${skills.length} search therm(s)`}
-        </Typography>
+        <Typography className={classes.secondaryHeading}>{levelList}</Typography>
       </ExpansionPanelSummary>
       <ExpansionPanelDetails>
-        <Typography>
-          {skillsList}
-        </Typography>
+        <Typography>Latest Milestone: -</Typography>
       </ExpansionPanelDetails>
+      <Divider />
+      <ExpansionPanelActions>
+        <Button size="small" color="primary">
+          View Profile
+        </Button>
+      </ExpansionPanelActions>
     </ExpansionPanel>
   );
 }
