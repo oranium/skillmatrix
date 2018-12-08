1a168ba1aaf7ddd886e52bf57af91529a1d991fa
techware01 <Anonymus1404@gmail.com>
2018-12-08 20:03:43 +0100

    add than function to axious to return data directly instead of entire api response.

--- src/frontend/src/rest/Rest.js
+++ src/frontend/src/rest/Rest.js
@@ -32,25 +32,26 @@ class RestCom {
         'Content-Type': 'application/json',
       },
     };
-    const response = await axios.post(this.restApi, this.data, headers).catch((error) => {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        throw new Error(errorCodesToErrorMsg(error.response.status));
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        throw new Error(
-          'An error occured while connecting to the server. Please check your Internet connection.',
-        );
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        throw new Error(errorCodesToErrorMsg(520));
-      }
-    });
-
-    return response;
+    return axios
+      .post(this.restApi, this.data, headers)
+      .then(ServerResponse => ServerResponse)
+      .catch((error) => {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          throw new Error(errorCodesToErrorMsg(error.response.status));
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          throw new Error(
+            'An error occured while connecting to the server. Please check your Internet connection.',
+          );
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          throw new Error(errorCodesToErrorMsg(520));
+        }
+      });
   }
 }
 export default RestCom;
