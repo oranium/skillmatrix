b0def659274230e427d2e374d9ad0664757d56e7
Aron Gaden <aron.gaden@gmail.com>
2018-12-09 16:27:02 +0100

    controller.py: call jsonable() for add_milestone return

--- src/backend/src/controller/controller.py
+++ src/backend/src/controller/controller.py
@@ -50,7 +50,7 @@ class Controller:
             database_controller.add_milestone(username, skill, date, comment, level)
             user_skills = database_controller.get_skills(username)
             name = authentication_controller.get_name(username)
-            return ProfileModel(username, name, user_skills)
+            return ProfileModel(username, name, user_skills).jsonable()
         raise PermissionError   
 
     @staticmethod
