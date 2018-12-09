5ce5e3f8857983e1fdbb5ee5dbd8976cc30747e5
Aron Gaden <aron.gaden@gmail.com>
2018-12-09 12:51:23 +0100

    api/: return status code 401 if user is not logged in

--- src/backend/src/api/logout.py
+++ src/backend/src/api/logout.py
@@ -15,6 +15,8 @@ class Logout(Resource):
                 return Response(status=401)
             json.dumps(controller.logout(self, args["username"]))
             return Response(status=200)
+        except PermissionError:
+            return Response(status=401)
         except Exception:
             return Response(status=520)
 
--- src/backend/src/api/set_skill.py
+++ src/backend/src/api/set_skill.py
@@ -14,12 +14,12 @@ class SetSkill(Resource):
         parser.add_argument("skills", type=dict)
         args = parser.parse_args()
         try:
-            if not controller.is_connected(args["username"]):
-                return Response(status=401)
             message = json.dumps(controller.set_skills(self, args["username"], args["skills"]))
             return Response(message, status=200, mimetype="application/json")
         except TimeoutError:
             return Response(status=504)
+        except PermissionError:
+            return Response(status=401)
 
     def options(self):
         pass
