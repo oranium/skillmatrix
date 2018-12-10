70b6d4589f631167ef22a3153c8d1013ea3461cc
Aron Gaden <aron.gaden@gmail.com>
2018-12-10 19:53:09 +0100

    rewrite tests for models

--- src/backend/test/test_model.py
+++ src/backend/test/test_model.py
@@ -16,65 +16,53 @@ class TestModel(unittest.TestCase):
         self.assertRaises(NotImplementedError, Model.jsonable, self)
 
 
-class TestSkillModel(unittest.TestCase):
-    def setUp(self):
-        self.TEST_SKILL_MODEL = SkillModel("Java", 3)
-        self.SKILL_JSON = json.dumps(dict(skillname="Java", level=3, milestones=list()))
-
-    def test_to_json(self):
-        self.assertEqual(self.SKILL_JSON, self.TEST_SKILL_MODEL.jsonable())
-
-
-class TestMilestoneModel(unittest.TestCase):
-    def setUp(self):
-        self.TEST_MILESTONE_MODEL = MilestoneModel(datetime.date(2018, 11, 30), "Created humongous Python backend")
-        self.MILESTONE_JSON = json.dumps(dict(date=datetime.date(2018, 11, 30).isoformat(),
-                                              comment="Created humongous Python backend"))
-
-    def test_to_json(self):
-        self.assertEqual(self.MILESTONE_JSON, self.TEST_MILESTONE_MODEL.jsonable())
-
-
 class TestProfileModel(unittest.TestCase):
 
     def setUp(self):
-        test_milestone1 = MilestoneModel(datetime.datetime(2018, 11, 30), "comment")
-        test_milestone2 = MilestoneModel(datetime.datetime(2014, 1, 22), "other comment")
-        test_skill = SkillModel("Java", 3, [test_milestone2, test_milestone1])
-        self.TEST_PROFILE_MODEL = ProfileModel("Aron", [test_skill])
-        self.PROFILE_JSON = json.dumps(dict(username="Aron", skills=[test_skill.jsonable()]))
+        self.TEST_MILESTONE_MODEL = MilestoneModel(datetime.date(2018, 11, 30), "Created humongous Python backend", 3)
+        self.TEST_SKILL_MODEL = SkillModel("Java", 3, [self.TEST_MILESTONE_MODEL])
+        self.TEST_PROFILE_MODEL = ProfileModel("Aron", "Aron", [self.TEST_SKILL_MODEL])
+        self.MILESTONE_JSONABLE = dict(date="2018-11-30", comment="Created humongous Python backend", level=3)
+        self.SKILL_JSONABLE = dict(skillname="Java", level=3, milestones=[self.TEST_MILESTONE_MODEL.jsonable()])
+        self.PROFILE_JSONABLE = dict(username="Aron", name="Aron", skills=[self.TEST_SKILL_MODEL.jsonable()])
 
     # TODO
-    def test_to_json(self):
-        self.assertEqual(self.PROFILE_JSON, self.TEST_PROFILE_MODEL.jsonable())
+    def test_milestone_jsonable(self):
+        self.assertEqual(self.MILESTONE_JSONABLE, self.TEST_MILESTONE_MODEL.jsonable())
+
+    def test_skill_jsonable(self):
+        self.assertEqual(self.SKILL_JSONABLE, self.TEST_SKILL_MODEL.jsonable())
+
+    def test_profile_jsonable(self):
+        self.assertEqual(self.PROFILE_JSONABLE, self.TEST_PROFILE_MODEL.jsonable())
 
 
 class TestSearchModel(unittest.TestCase):
 
     def setUp(self):
         search_query = {"Java": 3, "Python": 2}
-        search_results = {"has_some": [ProfileModel("Aron", [SkillModel("Java", 3)])],
-                          "has_all": [ProfileModel("Peter", [SkillModel("Java", 4), SkillModel("Python", 5)])]}
+        search_results = {"has_some": [ProfileModel("Aron", "Aron", [SkillModel("Java", 3)])],
+                          "has_all": [ProfileModel("Peter", "Peter", [SkillModel("Java", 4), SkillModel("Python", 5)])]}
         self.TEST_SEARCH = SearchModel(search_query, search_results)
-        self.SEARCH_JSON = json.dumps(dict
-                                      (query=search_query,
-                                       has_all=[ProfileModel
-                                                ("Peter", [SkillModel("Java", 4), SkillModel("Python", 5)])
-                                       .jsonable()],
-                                       has_some=[ProfileModel
-                                                 ("Aron", [SkillModel("Java", 3)])
-                                       .jsonable()]
-                                       )
-                                      )
+        self.SEARCH_JSONABLE = dict(query=search_query,
+                                    results=dict(
+                                        has_all=
+                                        [ProfileModel("Peter", "Peter",
+                                                      [SkillModel("Java", 4), SkillModel("Python", 5)])
+                                             .jsonable()],
+                                        has_some=
+                                        [ProfileModel("Aron", "Aron", [SkillModel("Java", 3)])
+                                             .jsonable()]
+                                    ))
 
-    def test_to_json(self):
-        self.assertEqual(self.SEARCH_JSON, self.TEST_SEARCH.jsonable())
+    def test_jsonable(self):
+        self.assertEqual(self.SEARCH_JSONABLE, self.TEST_SEARCH.jsonable())
 
 
 class TestLogoutModel(unittest.TestCase):
     def setUp(self):
         self.TEST_LOGOUT_MODEL = LogoutModel("some_name")
-        self.LOGOUT_JSON = json.dumps(dict(user="some_name"))
+        self.LOGOUT_JSONABLE = dict(user="some_name")
 
-    def test_to_json(self):
-        self.assertEqual(self.LOGOUT_JSON, self.TEST_LOGOUT_MODEL.jsonable())
+    def test_jsonable(self):
+        self.assertEqual(self.LOGOUT_JSONABLE, self.TEST_LOGOUT_MODEL.jsonable())
