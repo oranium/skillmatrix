0fc5121105c513b2acc9f9e07a8c42b120fdc25e
oranium <aron.gaden@gmail.com>
2018-12-14 11:43:31 +0100

    update SkillModel, close #132

--- src/backend/src/model/skill_model.py
+++ src/backend/src/model/skill_model.py
@@ -6,7 +6,8 @@ class SkillModel(Model):
 
     """This class converts a skill to a JSON which is a component of a Profile """
 
-    def __init__(self, skill_name, level, milestones=None):
+    def __init__(self, skill_name, level, category=None, milestones=None):
+        self.category = category
         self.skill_name = skill_name
         self.level = level
         self.milestones = milestones
@@ -16,4 +17,4 @@ class SkillModel(Model):
         if self.milestones:
             for milestone in self.milestones:
                 jsonable_milestones.append(milestone.jsonable())
-        return dict(skillname=self.skill_name, level=self.level, milestones=jsonable_milestones)
+        return dict(skillname=self.skill_name, level=self.level, category=self.category, milestones=jsonable_milestones)
