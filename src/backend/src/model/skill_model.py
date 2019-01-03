b7692bbc1e1340497bd3b92732ec06a737ed589e
Aron Gaden <aron.gaden@gmail.com>
2019-01-03 01:39:20 +0100

    fix typo, level is no longer obligatory

--- src/backend/src/model/skill_model.py
+++ src/backend/src/model/skill_model.py
@@ -6,7 +6,7 @@ class SkillModel(Model):
 
     """This class converts a skill to a JSON which is a component of a Profile """
 
-    def __init__(self, skill_name, level, subcategories=[], root=False, milestones=[]):
+    def __init__(self, skill_name, level=None, subcategories=[], root=False, milestones=[]):
         self.subcategories = subcategories
         self.skill_name = skill_name
         self.level = level
@@ -22,6 +22,6 @@ class SkillModel(Model):
             jsonable_milestones.append(milestone.jsonable())
         return dict(skillname=self.skill_name,
                     level=self.level,
-                    subcategories=self.category,
+                    subcategories=self.subcategories,
                     root=self.root,
                     milestones=jsonable_milestones)
