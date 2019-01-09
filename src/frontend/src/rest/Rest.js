76c5cdea8eed94f233c89409de2691a3f9dae0c7
techware01 <Anonymus1404@gmail.com>
2019-01-09 01:12:07 +0100

    put functions inside RestCom class as static functions

--- src/frontend/src/rest/Rest.js
+++ src/frontend/src/rest/Rest.js
@@ -7,50 +7,9 @@ const config = require('../config.json');
 
 const { APISERVER } = config;
 
-const errorCodesToErrorMsg = (errorCode) => {
-  switch (errorCode) {
-    case 400:
-      return 'Wrong login credentials.';
-    case 401:
-      return 'You need to be logged in to view this page.';
-    case 404:
-      return 'Couldnt connect to Server. Please try again.';
-    case 504:
-      return 'Active Directory timeout. Please try again.';
-    case 520:
-      return 'An unknown error occured. Please try again.';
-    default:
-      return 'An unknown error occured. Please try again.';
-  }
-};
-
-const errorHandling = (error) => {
-  if (error.response) {
-    // The request was made and the server responded with a status code
-    // that falls out of the range of 2xx
-    const errorMsg = errorCodesToErrorMsg(error.response.status);
-    if (error.response.status === 401) {
-      // user is not logged in and has no permission to do the request
-      store.dispatch(resetState);
-      store.dispatch(setLoginError(errorMsg));
-    } else {
-      throw new Error(errorMsg);
-    }
-  } else if (error.request) {
-    // The request was made but no response was received
-    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-    // http.ClientRequest in node.js
-    throw new Error(
-      'An error occured while connecting to the server. Please check your Internet connection.',
-    );
-  } else {
-    // Something happened in setting up the request that triggered an Error
-    throw new Error(errorCodesToErrorMsg(520));
-  }
-};
-
 class RestCom {
   constructor(restPoint, data = '') {
+    // serveradress + rest end point
     this.restApi = APISERVER + restPoint;
     this.data = JSON.stringify(data);
     this.headers = {
@@ -59,14 +18,55 @@ class RestCom {
         'Content-Type': 'application/json',
       },
     };
+    // RestCom is constructed every time a request is made
+    // => show loading indicator
     store.dispatch(toggleSpinner(true));
   }
 
-  static handleThen(ServerResponse) {
-    return ServerResponse.data;
+  static errorCodesToErrorMsg(errorCode) {
+    switch (errorCode) {
+      case 400:
+        return 'Wrong login credentials.';
+      case 401:
+        return 'You need to be logged in to view this page.';
+      case 404:
+        return 'Couldnt connect to Server. Please try again.';
+      case 504:
+        return 'Active Directory timeout. Please try again.';
+      case 520:
+        return 'An unknown error occured. Please try again.';
+      default:
+        return 'An unknown error occured. Please try again.';
+    }
+  }
+
+  static handleError(error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      const errorMsg = RestCom.errorCodesToErrorMsg(error.response.status);
+      if (error.response.status === 401) {
+        // user is not logged in and has no permission to do the request
+        store.dispatch(resetState);
+        store.dispatch(setLoginError(errorMsg));
+      } else {
+        throw new Error(errorMsg);
+      }
+    } else if (error.request) {
+      // The request was made but no response was received
+      // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      throw new Error(
+        'An error occured while connecting to the server. Please check your Internet connection.',
+      );
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      throw new Error(RestCom.errorCodesToErrorMsg(520));
+    }
   }
 
   static endLoading() {
+    // hide loading spinner after request has finished
     store.dispatch(toggleSpinner(false));
   }
 
@@ -75,7 +75,7 @@ class RestCom {
       .post(this.restApi, this.data, this.headers)
       .then(ServerResponse => ServerResponse.data)
       .finally(RestCom.endLoading)
-      .catch(error => errorHandling(error));
+      .catch(error => RestCom.handleError(error));
   }
 
   async get() {
@@ -83,7 +83,7 @@ class RestCom {
       .get(this.restApi, this.headers)
       .then(ServerResponse => RestCom.handleThen(ServerResponse))
       .finally(RestCom.endLoading)
-      .catch(error => errorHandling(error));
+      .catch(error => RestCom.handleError(error));
   }
 
   async delete() {
@@ -91,7 +91,7 @@ class RestCom {
       .delete(this.restApi, this.headers)
       .then(ServerResponse => RestCom.handleThen(ServerResponse))
       .finally(RestCom.endLoading)
-      .catch(error => errorHandling(error));
+      .catch(error => RestCom.handleError(error));
   }
 }
 export default RestCom;
