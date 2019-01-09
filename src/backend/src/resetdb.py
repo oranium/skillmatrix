e16a7d2fc2535e0a29b9beed3ae0f6df28c32370
wpertsch <willy.pertsch@gmail.com>
2019-01-09 23:13:24 +0100

    fixing path to nullable

--- src/backend/src/resetdb.py
+++ src/backend/src/resetdb.py
@@ -50,7 +50,7 @@ class MilestoneAssociation(Base):
 class Skill(Base):
     __tablename__ = 'skill'
     id = Column(Integer, primary_key=True)
-    path = Column(Text, primary_key=True)
+    path = Column(Text, nullable=False)
     name = Column(String(255), nullable=False)
     root = Column(Boolean, unique=False, default=False)
     skill_association = relationship("Association", back_populates="skill_assoc")
