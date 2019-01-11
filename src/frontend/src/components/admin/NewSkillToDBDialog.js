96aefcd40a07645c85de41096e6350bb8b9c730b
techware01 <Anonymus1404@gmail.com>
2019-01-11 19:33:50 +0100

    enterly update newSkillToDBDialog

--- src/frontend/src/components/admin/NewSkillToDBDialog.js
+++ src/frontend/src/components/admin/NewSkillToDBDialog.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 
 // components
 import SingleSkillSelect from 'components/common/SingleSkillSelect';
+import GuidelineInputCard from 'components/admin/GuidelineInputCard';
 
 // material-ui
 import Button from '@material-ui/core/Button';
@@ -14,15 +15,16 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { withStyles } from '@material-ui/core/styles';
-import classNames from 'classnames';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import Card from '@material-ui/core/Card';
 
 // redux
 import store from 'Store';
-import { closeProfileDialog, resetForm, setError } from 'actions';
+import {
+  closeProfileDialog,
+  resetForm,
+  setError,
+  toggleConfirmDialog,
+  setSkillName,
+} from 'actions';
 
 // Rest
 import RestPoints from 'rest/Init';
@@ -37,57 +39,37 @@ const styles = theme => ({
   dialogcontent: {
     fullWidth: true,
     maxWidth: 'none',
+    marginBottom: '10px',
   },
 });
 
 class FormDialog extends Component {
-  state = {
-    open: false,
-    skillname: '',
-    guideline: {
-      '1': 'Insufficient',
-      '2': 'Sufficient/Below Average',
-      '3': 'Satisfactory / Average',
-      '4': 'Good',
-      '5': 'Excellent',
-    },
-  };
-
-  handleChange = event => {
+  //
+  handleSkillNameChange = event => {
     const skillname = event.target.value;
     if (skillname.includes('/')) {
       store.dispatch(setError('You can\'t use "/" in a skillname.'));
-      event.target.value = this.state.skillname;
+      // remove "/" from input value
+      event.target.value = skillname.replace('/','');
     } else {
-      this.setState({
-        skillname,
-      });
+      store.dispatch(setSkillName(skillname));
     }
   };
 
-  handleGuidelineChange(level, event) {
-    const newGuideline = event.target.value;
-    this.setState({
-      guideline: { ...this.state.guideline, [level]: newGuideline },
-    });
-  }
-
-  handleClose = () => {
-    store.dispatch(resetForm);
-    store.dispatch(closeProfileDialog);
-  };
-
+  // open confirmation dialog
   handleClickOpen = () => {
-    this.setState({ open: true });
+    store.dispatch(toggleConfirmDialog(true));
   };
+  // close confirmation dialog
   handleClickClose = () => {
-    this.setState({ open: false });
+    store.dispatch(toggleConfirmDialog(false));
   };
 
+  // send new skill to database
   async handleSubmit() {
     const state = store.getState();
     const category = state.formState.singleselect.value;
-    const { skillname, guideline } = this.state;
+    const { skillname, guideline } = state.newSkillToDBDialog;
 
     //build whole path for new skill
     var skillpath = '';
@@ -107,9 +89,6 @@ class FormDialog extends Component {
       guideline,
     };
 
-    console.log(newSkillRequest);
-    console.log(newGuidelineRequest);
-
     const RestSkillRequest = new RestCom(RestPoints.createSkill, newSkillRequest);
     const RestGuidlineRequest = new RestCom(RestPoints.setGuidelines, newGuidelineRequest);
 
@@ -128,10 +107,52 @@ class FormDialog extends Component {
     await updateAllSkills();
   }
 
+  // close NewSkillToDBDialog
+  handleClose = () => {
+    store.dispatch(resetForm);
+    store.dispatch(closeProfileDialog);
+  };
+
   render() {
     const state = store.getState();
     const { showDialog } = state.profile;
+    const { singleselect } = state.formState;
+    const { confirmDialogOpen, skillname } = state.newSkillToDBDialog;
+
+    // dialog that is shown when user leafs category emtpy
+    const confirmDialog = (
+      <div className="confirm-dialog">
+        <Dialog
+          open={confirmDialogOpen}
+          onClose={() => this.handleClose()}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {'Are you sure you want to add "' + skillname + '" as a new category?'}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              You didn't choose a category. This means you will add the skill as new main category
+              which has no attributes.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClickClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={() => this.handleSubmit()} color="primary" autoFocus>
+              Submit
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+
+    // don't show guideline inputs when user wants to add category
+    const showGuidelineInput = singleselect.value !== '';
     const { classes } = this.props;
+
     return (
       <div className={classes.root}>
         <Dialog
@@ -152,74 +173,14 @@ class FormDialog extends Component {
               <TextField
                 id="outlined-with-placeholder"
                 label="skill name"
-                placeholder="write the skill to add"
+                placeholder="Name the skill you want to add"
                 className={classes.textField}
                 margin="normal"
                 variant="outlined"
-                onChange={event => this.handleChange(event)}
+                onChange={event => this.handleSkillNameChange(event)}
                 fullWidth
               />
-              <Card>
-                <List
-                  component="nav"
-                  subheader={
-                    <ListSubheader component="div">
-                      (Optional) add guideline to the skill
-                    </ListSubheader>
-                  }
-                >
-                  <ListItem>
-                    <TextField
-                      label="Level 1: "
-                      id="level1"
-                      className={classNames(classes.margin, classes.textField)}
-                      placeholder={this.state.guideline['1']}
-                      onChange={event => this.handleGuidelineChange('1', event)}
-                      fullWidth
-                    />
-                  </ListItem>
-                  <ListItem>
-                    <TextField
-                      label="Level 2"
-                      id="level2"
-                      className={classNames(classes.margin, classes.textField)}
-                      placeholder={this.state.guideline['2']}
-                      onChange={event => this.handleGuidelineChange('2', event)}
-                      fullWidth
-                    />
-                  </ListItem>
-                  <ListItem>
-                    <TextField
-                      label="Level 3: "
-                      id="level3"
-                      className={classNames(classes.margin, classes.textField)}
-                      placeholder={this.state.guideline['3']}
-                      onChange={event => this.handleGuidelineChange('3', event)}
-                      fullWidth
-                    />
-                  </ListItem>
-                  <ListItem>
-                    <TextField
-                      label="Level 4: "
-                      id="level4"
-                      className={classNames(classes.margin, classes.textField)}
-                      placeholder={this.state.guideline['4']}
-                      onChange={event => this.handleGuidelineChange('4', event)}
-                      fullWidth
-                    />
-                  </ListItem>
-                  <ListItem>
-                    <TextField
-                      label="Level 5: "
-                      id="level5"
-                      className={classNames(classes.margin, classes.textField)}
-                      placeholder={this.state.guideline['5']}
-                      onChange={event => this.handleGuidelineChange('5', event)}
-                      fullWidth
-                    />
-                  </ListItem>
-                </List>
-              </Card>
+              {showGuidelineInput && <GuidelineInputCard />}
             </DialogContent>
             <DialogActions>
               <Button onClick={this.handleClose} color="primary">
@@ -237,32 +198,7 @@ class FormDialog extends Component {
             </DialogActions>
           </div>
         </Dialog>
-        <div className="confirm-dialog">
-          <Dialog
-            open={this.state.open}
-            onClose={this.handleClose}
-            aria-labelledby="alert-dialog-title"
-            aria-describedby="alert-dialog-description"
-          >
-            <DialogTitle id="alert-dialog-title">
-              {'Are you sure you want to add "' + this.state.skillname + '" as a new category?'}
-            </DialogTitle>
-            <DialogContent>
-              <DialogContentText id="alert-dialog-description">
-                You didn't choose a category. This means you will add the skill as new main category
-                which has no attributes.
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={this.handleClickClose} color="primary">
-                Cancel
-              </Button>
-              <Button onClick={() => this.handleSubmit()} color="primary" autoFocus>
-                Submit
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </div>
+        {confirmDialog}
       </div>
     );
   }
